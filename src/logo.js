3b0b05a3f8dc3d497bcb71bb1c6d4d484fe2901e
C2307 <166719934+C2307@users.noreply.github.com>
2025-12-23 18:40:48 +0800

    change Website main

---
+++ src/logo.js
@@ -0,0 +1,38 @@
+// 漂浮文本代码
+window.onload = function () {
+    //获取元素
+    var div1 = document.getElementById('div1');
+    var w = document.documentElement.clientWidth;
+    var h = document.documentElement.clientHeight;
+    var gox = 1;
+    var goy = 1;
+    var a=0;
+    //广告漂浮
+    function move() {
+        var x = div1.offsetLeft;
+        var y = div1.offsetTop;
+        if (x > w - 200 || x < 0) gox = -gox;
+        div1.style.left = x + (5 * gox) + 'px';
+        if (y > h - 80 || y < 0) goy = -goy;
+        div1.style.top = y + (5 * goy) + 'px';
+    }
+    //漂浮定时器
+    var timer = setInterval(move, 25);
+    //移入移出时广告的浮动
+    div1.onmouseover = function () {
+        clearInterval(timer);
+    }
+    div1.onmouseout = function () {
+        timer = setInterval(move, 25);
+    }
+    //点击一次跳转到另一个页面，点击两次广告消失
+    div1.onclick = function() {
+        if (a === 0) {
+            var neww = window.open('_blank');
+            neww.location = "https://tuo.hfc2307.top/";
+            a++;
+        } else {
+            this.style.display = "none";
+        }
+    }
+}
